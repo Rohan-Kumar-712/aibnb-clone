5e826f946c3005e93a624fb989a1eec76fd5c4da
Rohan-Kumar-712 <rohankumar7128@gmail.com>
2022-02-08 22:51:44 +0530

    Created the Banner with adding more elements. Created the different type of cards which includes the destination about where to go, etc using props and simultaneously getting the data from the API. This consits of Small, Medium & Large Cards

--- Components/Banner.js
+++ Components/Banner.js
@@ -8,8 +8,12 @@ function Banner() {
                 src="https://links.papareact.com/0fm"
                 layout = "fill" objectFit = "cover"
             />
-            <div>
-                <p>Not sure where to go? P</p>
+            <div className='absolute top-1/2 w-full text-center'>
+            
+                <p className='text-sm sm:text-lg'> Not sure where to go? Perfect.</p>
+
+                <button className='bg-white text-purple-500 px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>I'm Flexible</button>
+
             </div>
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 import Header from '../Components/Header';
 import Banner from '../Components/Banner';
+import SmallCard from '../Components/SmallCard';
+import MediumCard from '../Components/MediumCard';
+import LargeCard from '../Components/LargeCard';
 
-export default function Home() {
+export default function Home({exploreData, cardsData}) {
   return (
     <div className="">
 
@@ -16,6 +19,57 @@ export default function Home() {
 
     <Banner />
 
+    <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+      <section className='pt-5'>
+        <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+        {/* Pulling some data from a server - API endpoints */}
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+
+        {exploreData?.map((item) => (
+          <SmallCard 
+          key={item.img}
+          img={item.img} distance={item.distance} location={item.location} />
+
+        ))}
+        </div>
+      </section>
+
+      <section>
+        <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+
+        <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+        {cardsData?.map(({img, title}) => (
+        <MediumCard key={img} img = {img} title = {title}/>
+        ))}
+        </div>  
+      </section>
+
+      <LargeCard
+        img="https://links.papareact.com/4cj"  
+        title="The Greatest Outdoors"
+        description= "Wishlist curated by Airbnb"
+        buttonText="Get Inspired"
+       />
+    </main>
+
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+
+  const cardsData = await fetch("https://links.papareact.com/zp1").then(
+    (res) => res.json()
+  );
+
+  return{
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
 }
