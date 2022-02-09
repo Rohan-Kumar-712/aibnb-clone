ac8f3e25ac2a552662b42e02e5742039c53fcb41
Rohan-Kumar-712 <rohankumar7128@gmail.com>
2022-02-09 21:31:57 +0530

    Completion of Front End Build

--- Components/LargeCard.js
+++ Components/LargeCard.js
@@ -6,6 +6,7 @@ function LargeCard({img, title, description, buttonText}) {
   <section className='relative py-16 cursor-pointer'>
     <div className='relative h-96 min-w-300'>
         <Image src={img}
+            alt='banner image'
             layout='fill' 
             objectFit='cover'
             className='rounded-2xl'
@@ -15,7 +16,7 @@ function LargeCard({img, title, description, buttonText}) {
     <div className='absolute top-32 left-12'>
         <h3 className='text-4xl mb-3w-64'>{title}</h3>
         <p>{description}</p>
-
+ 
         <button className='text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5 hover:scale-105 transition-transform duration-100 ease-out'>{buttonText}</button>
     </div>
   </section>
