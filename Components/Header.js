154872c8ce47ed875e87086a4eedce57a76a5e99
Rohan-Kumar-712 <rohankumar7128@gmail.com>
2022-02-13 21:05:44 +0530

    Added the schedule planner

--- Components/Header.js
+++ Components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from "next/image";
 import {GlobeAltIcon,
   MenuIcon,
@@ -6,8 +6,26 @@ import {GlobeAltIcon,
   UserIcon,
     SearchIcon} from '@heroicons/react/solid';
 import { Input } from 'postcss';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 function Header() {
+  const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const selectionRange = {
+    startDate:startDate,
+    endDate: endDate,
+    key: 'selection'
+  };
+
   return (
   <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
 
@@ -20,8 +38,12 @@ function Header() {
 
     {/* middle - search*/}
     <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-lg text-slate-800'>
-      <input className='flex-grow pl-5 bg-transparent outline-none' type="text" placeholder='start your search'/>
-      <SearchIcon className='hidden md:inline flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
+      <input 
+      value={searchInput}
+      onChange={(e) => setSearchInput(e.target.value)}
+      className='flex-grow pl-5 bg-transparent outline-none' 
+      type="text" placeholder='start your search'/>
+      <SearchIcon className='md:inline flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
     </div>
 
     {/* right */}
@@ -34,6 +56,17 @@ function Header() {
         <UserCircleIcon className='h-6'/>
       </div>
     </div>
+
+    {searchInput && (
+      <div>
+        <DateRangePicker
+         ranges={[selectionRange]}
+         minDate={new Date()}
+         rangeColors={["#FD5B61"]}
+         onChange={[handleSelect]}
+         />
+      </div>
+    )}
 
   </header>
   );
